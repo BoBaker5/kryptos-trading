77cc694b8d96df68d132d02fe98787b0e59ea5aa
BoBaker5 <bowenbaker5@gmail.com>
2025-01-12 23:41:08 -0500

    Create __init__.py

---
+++ backend/__init__.py
@@ -0,0 +1,9 @@
+from .demo_bot import DemoKrakenBot
+from .kraken_crypto_bot_ai import EnhancedKrakenCryptoBot
+from .bot_manager import BotManager
+
+__all__ = [
+    'DemoKrakenBot',
+    'EnhancedKrakenCryptoBot',
+    'BotManager'
+]
